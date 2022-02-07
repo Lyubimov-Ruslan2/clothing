ed9f8af54d29af4256f9663b6b3c99e7655de1a4
Lyubimov-Ruslan2 <ruslanlyubimov04@gmail.com>
2022-02-07 18:10:14 +0500

    added firebase database

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
-
+import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route exact path="/" element={<HomePage />} />
-        <Route path="/hats" element={<HatsPage />} />
+        <Route path="/shop" element={<ShopPage />} />
+        <Route path="/signin" element={<SignInAndSignUpPage />} />
       </Routes>
     </div>
   );
